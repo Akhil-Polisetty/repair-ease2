4d1d5986410ecf75c2059615023c8a8221ceaadb
Akhil-Polisetty <polisettiakhi72073@gmail.com>
2024-04-05 11:25:58 +0530

    after adding Link

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './login.css'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
 
 import axios from "axios";
